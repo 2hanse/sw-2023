78501745eb69686bd3cac3e65c249cc370845b46
lee hanse <prsuw113@naver.com>
2023-12-08 10:12:33 +0900

    commit-13

---
+++ python.13.py
@@ -0,0 +1,43 @@
+'''
+sum=0
+
+for i in range(1000):
+    i = i +sum
+    
+print("n : " ,i)
+print("sum : ",sum)
+
+'''
+
+
+sum=0
+
+for i in range(100):
+    i+=1
+    sum+=i
+    print("n : ", i)
+    print("sum : ", sum)
+    if sum>1000:
+        break
+print("last n: ",i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
